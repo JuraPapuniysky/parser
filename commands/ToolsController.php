dd43498909340747abe39bbb805a2e828896e88b
JuraPapuniysky <jurapapunitsky@gmail.com>
2018-01-15 13:52:59 +0200

    tools $search

--- commands/ToolsController.php
+++ commands/ToolsController.php
@@ -14,13 +14,13 @@ use yii\console\Controller;
 
 class ToolsController extends Controller
 {
-    public function actionFormatNames()
+    public function actionFormatNames($search)
     {
         $catalogs = Catalog::find()->all();
         $i = 1;
         foreach ($catalogs as $catalog){
             $oldName = $catalog->name;
-            $catalog->name = str_replace(', как доехать, адрес, телефон, сайт', '', $catalog->name);
+            $catalog->name = str_replace($search, '', $catalog->name);
             $catalog->save();
             echo $i. '. Catatalog name was changed '. $oldName .' to '. $catalog->name. "\n";
             $i++;
